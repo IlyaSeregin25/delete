afaea04b0cb7ad21acb9598c4dcbab5de6bef6cf
Ilya99 <Ilya99@mail.ru>
2026-01-05 23:29:06 +0400

    fix bags

--- src/components/pages/ContactsPage/ui/ContactsPage.jsx
+++ src/components/pages/ContactsPage/ui/ContactsPage.jsx
@@ -8,9 +8,9 @@ const ContactsPage = () => {
   return (
     <section className={styles.contacts}>
       <div className={`${styles.contacts__inner} container`}>
-        <h2 className={styles.contacts__title}>Контакты</h2>
+        <h2 className={styles.contacts__title}>Наши адреса</h2>
         <div className={styles.contacts__list}>
-          <div className={styles.contacts__card}>
+          {/*           <div className={styles.contacts__card}>
             <h4 className={styles.contacts__card_title}>Телефон</h4>
             <div className={styles.contacts__card_body}>
               <Phones icon={false} />
@@ -22,8 +22,9 @@ const ContactsPage = () => {
               <Socials />
             </div>
           </div>
+ */}
           <div className={`${styles.contacts__card} ${styles.contacts__card_wide}`}>
-            <h4 className={styles.contacts__card_title}>Наши адреса</h4>
+            {/* <h4 className={styles.contacts__card_title}>Наши адреса</h4> */}
             <div className={styles.contacts__card_body}>
               <Addresses withImage={true} />
             </div>
--- src/components/ui/TeacherCard/ui/TeacherCard.jsx
+++ src/components/ui/TeacherCard/ui/TeacherCard.jsx
@@ -1,7 +1,7 @@
 import styles from './style.module.css';
 
 const TeacherCard = ({ data }) => {
-  const { title, mainImage } = data;
+  const { title, ageCategory, mainImage } = data;
 
   return (
     <article className={styles.card}>
@@ -10,6 +10,7 @@ const TeacherCard = ({ data }) => {
       </div>
       <section className={styles.card__description}>
         <h3 className={styles.card__title}>{title}</h3>
+        <p className={styles.card__subtitle}>{ageCategory}</p>
       </section>
     </article>
   );
